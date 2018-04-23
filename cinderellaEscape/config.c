067369cc1f6afb83c7af137c0ee0ef1aa736ac88
fherbine <felix.herbinet@yahoo.com>
2018-04-23 23:17:05 +0200

    Adding init_all() function in config.c

--- cinderellaEscape/config.c
+++ cinderellaEscape/config.c
@@ -1,1 +1,33 @@
 #include "cinderella.h"
+
+/**
+ *      This file contain configuration functions.
+**/
+
+
+/// This function init allegro itself, keyboard, mouse & graphical mode.
+
+int     init_all(void)
+{
+    allegro_init();
+
+    if(install_keyboard() == -1)
+    {
+        allegro_message("An error occured: %s\n", allegro_error);
+        return (-1);
+    }
+    if(install_mouse() == -1)
+    {
+        allegro_message("An error occured: %s\n", allegro_error);
+        return (-1);
+    }
+
+    set_color_depth(desktop_color_depth());
+    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED, 800, 600, 0, 0) != 0)
+    {
+        set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
+        allegro_message("Cannot set video mode : %s\n", allegro_error);
+        return (-1);
+    }
+    return (1);
+}
