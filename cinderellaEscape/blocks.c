4dbf264b20e82d9c9df9efbaab62f1b6ca07b02b
fherbine <felix.herbinet@yahoo.com>
2018-04-23 23:19:04 +0200

    Adding backgroundColor() function

--- cinderellaEscape/blocks.c
+++ cinderellaEscape/blocks.c
@@ -1,1 +1,11 @@
 #include "cinderella.h"
+
+void    backgroundColor(int color)
+{
+    BITMAP *buffer;
+
+    buffer = create_bitmap(SCREEN_W, SCREEN_H);
+    clear_bitmap(buffer);
+    rectfill(buffer, 0, 0, SCREEN_W, SCREEN_H, color);
+    blit(buffer, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
+}
