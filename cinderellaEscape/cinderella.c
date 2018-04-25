15bad912e8b0c0143f9c624cade7cba06017153f
fherbine <felix.herbinet@yahoo.com>
2018-04-25 18:36:56 +0200

    Adding banner

--- cinderellaEscape/cinderella.c
+++ cinderellaEscape/cinderella.c
@@ -6,12 +6,12 @@ BITMAP *game(BITMAP *buff)
 }
 **/
 
-BITMAP *menu_func(BITMAP *menu, int *status, t_box *boxes)
+BITMAP *menu_func(BITMAP *menu, int *status, t_box *m_buts)
 {
-    boxes[0] = buttonStatusUpdate(boxes[0]);
-    if ((boxes[0]).but_status == 2)
+    m_buts[0] = buttonStatusUpdate(m_buts[0]);
+    if ((m_buts[0]).but_status == 2)
       *status = 1;
-    menu = put_box(boxes[0], menu);
+    menu = put_box(m_buts[0], menu);
 }
 
 void cind(void)
@@ -19,11 +19,11 @@ void cind(void)
     BITMAP  *win;
     BITMAP  *game;
     BITMAP  *menu;
-    t_box   *boxes;
+    t_box   *m_buts;
     int     *status;
 
 
-    if (!(boxes = (t_box *)malloc(sizeof(t_box) * 20))) /// 20 !!
+    if (!(m_buts = (t_box *)malloc(sizeof(t_box) * 20))) /// 20 !!
         exit(EXIT_FAILURE);
 
     init_all();
@@ -37,8 +37,8 @@ void cind(void)
     clear_bitmap(game);
     game = backgroundColor(WHITE, game);
     menu = backgroundColor(WHITE, menu);
-    boxes[0] = newButton(200, 400, "Nouvelle partie");
-    menu = put_box(boxes[0], menu);
+    m_buts[0] = newButton(200, 400, "Nouvelle partie");
+    menu = put_box(m_buts[0], menu);
     win = menu;
     *status = 0;
     blit(win, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
@@ -48,7 +48,7 @@ void cind(void)
     {
         win = (*status == 0) ? menu : game;
         if (*status == 0)
-            menu = menu_func(menu, status, boxes);
+            menu = menu_func(menu, status, m_buts);
         blit(win, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
     }
 }
