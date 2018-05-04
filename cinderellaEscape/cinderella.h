2ea889aee69c277bc4ad40e492c298ec22764a7d
fherbine <felix.herbinet@yahoo.com>
2018-05-04 13:08:34 +0200

    Adding help_init

--- cinderellaEscape/cinderella.h
+++ cinderellaEscape/cinderella.h
@@ -47,6 +47,8 @@ void        game_routine(t_page *game);
 void        game_func(t_page *game, int *status);
 void        clear_lvl(t_page *game);
 
+t_page      *help_init(void);
+
 t_text      *new_txt(int x1, int y1, int x2, int y2);
 void        read_buf(t_text *editor, t_page *game);
 
--- cinderellaEscape/help.c
+++ cinderellaEscape/help.c
@@ -1,1 +1,12 @@
 #include "cinderella.h"
+
+t_page  *help_init(void)
+{
+    t_page *help;
+
+    if (!(help = (t_page *)malloc(sizeof(t_page))))
+        exit(EXIT_FAILURE);
+    if (!(help->buts = (t_box *)malloc(sizeof(t_box) * 10))) /// 10 !!
+        exit(EXIT_FAILURE);
+    return (help);
+}
