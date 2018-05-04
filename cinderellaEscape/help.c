cc29a5453f9a21026f70d149653ba5ee433f1672
fherbine <felix.herbinet@yahoo.com>
2018-05-04 13:51:29 +0200

    Finish implementation of help page

--- cinderellaEscape/help.c
+++ cinderellaEscape/help.c
@@ -16,30 +16,37 @@ void    help_routine(t_page *help)
     help->win = create_bitmap(SCREEN_W, SCREEN_H);
     clear_bitmap(help->win);
     help->win = backgroundImg("imgs/bg2.bmp", help->win);
-    (help->boxes)[0] = newButton(200, 50, "Menu");
-    (help->boxes)[1] = newButton(300, 50, "Jeu");
+    (help->boxes)[0] = init_box(80, 15, 720, 585);
+    (help->boxes)[0].bg_color = WHITE;
+    (help->boxes)[0].borders = BLACK;
+    (help->boxes)[1] = newButton(400, 50, "Menu");
+    (help->boxes)[2] = newButton(550, 50, "Jeu");
     help->win = put_box(help->boxes[0], help->win);
     help->win = put_box(help->boxes[1], help->win);
-    //menu->win = add_alph_bmp(menu->win, "imgs/banner.bmp", 200, 90, 300, 20);
+    help->win = put_box(help->boxes[2], help->win);
+    help->win = add_alph_bmp(help->win, "imgs/banner.bmp", 200, 90, 100, 40);
+    line(help->win, 100, 140, 700, 140, BLACK);
 }
 
 void help_func(t_page *help, int *status)
 {
-    help->boxes[0] = buttonStatusUpdate(help->boxes[0]);
-    if ((help->boxes[0]).but_status == 2)
+    //help->win = put_box(help->boxes[0], help->win);
+
+    help->boxes[1] = buttonStatusUpdate(help->boxes[1]);
+    if ((help->boxes[1]).but_status == 2)
     {
         *status = 0;
         destroy_bitmap(help->win);
         return;
     }
-    help->win = put_box(help->boxes[0], help->win);
+    help->win = put_box(help->boxes[1], help->win);
 
-    help->boxes[1] = buttonStatusUpdate(help->boxes[1]);
-    if ((help->boxes[1]).but_status == 2)
+    help->boxes[2] = buttonStatusUpdate(help->boxes[2]);
+    if ((help->boxes[2]).but_status == 2)
     {
         *status = 1;
         destroy_bitmap(help->win);
         return;
     }
-    help->win = put_box(help->boxes[1], help->win);
+    help->win = put_box(help->boxes[2], help->win);
 }
